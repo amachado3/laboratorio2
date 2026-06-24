6e3546030523434abdd83fa35da4aae96f4f0e74
glibertingremate <glibert@ingremate.com>
2026-06-24 19:44:57 +0100

    remove gerar_chave.py

--- Q4/GillesLibert/gerar_chave.py
+++
@@ -1,6 +0,0 @@
-from cryptography.fernet import Fernet
-chave = Fernet.generate_key()
-with open("Q4/GillesLibert/chave.key", "wb") as f:
-    f.write(chave)
-
-print("Chave gerada e gravada em chave.key")
